6d58dd9cf9378956e97d9d19514e1b2991a120e4
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-11-05 20:51:17 +0530

    ex 5.2

---
+++ Week-5/Ex 5.2/index.js
@@ -0,0 +1,20 @@
+const vowelCount = (str) => {
+    str = str.toLowerCase()
+    const VOWELS = ['a', 'e', 'i', 'o', 'u'];
+    let vowelMap = new Map();
+    for (let ele of str) {
+        if (VOWELS.includes(ele)) {
+            if (vowelMap.has(ele)) {
+                vowelMap.set(ele, vowelMap.get(ele) + 1);
+            }
+            else {
+                vowelMap.set(ele, 1)
+            }
+        }
+    }
+
+    console.log(vowelMap);
+}
+
+
+vowelCount('aeiouaeiouio')
